83abdb4eabdb400919a3340883f35bbb193710b5
SK.Liu <sk.liu0101@gmail.com>
2019-08-26 00:25:13 +0800

    add two test

---
+++ TennisGame/TennisGame.cs
@@ -0,0 +1,21 @@
+﻿namespace TennisGame
+{
+    public class TennisGame
+    {
+        private int _firstPlayerScore;
+
+        public void FirstPlayerScore()
+        {
+            _firstPlayerScore++;
+        }
+
+        public string Score()
+        {
+            if (_firstPlayerScore == 1)
+            {
+                return "Fifteen Love";
+            }
+            return "Love All";
+        }
+    }
+}
--- TennisGame/UnitTest1.cs
+++ TennisGame/UnitTest1.cs
@@ -4,11 +4,26 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace TennisGame
 {
     [TestClass]
-    public class UnitTest1
+    public class TennisGameTests
     {
+        protected internal TennisGame tennisGame = new TennisGame();
+
         [TestMethod]
-        public void TestMethod1()
+        public void Fifteen_Love()
         {
+            tennisGame.FirstPlayerScore();
+            ScoreShouldBe("Fifteen Love");
+        }
+
+        [TestMethod]
+        public void Love_All()
+        {
+            ScoreShouldBe("Love All");
+        }
+
+        private void ScoreShouldBe(string lookup)
+        {
+            Assert.AreEqual(lookup, tennisGame.Score());
         }
     }
 }
